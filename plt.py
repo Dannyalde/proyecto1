df43f50a48260dbf81812a26ec1f9f701310e09d
Danny <da.jimenezm@uniandes.edu.co>
2024-07-16 10:28:55 -0500

    modifique mi grafica

--- plt.py
+++ plt.py
@@ -3,8 +3,9 @@ import numpy as np
 
 x = np.linspace(-5,5,100)
 y = x**3
-plt.plot(x,y)
-plt.title("Función en python")
+plt.plot(x,y, "r--", label = "x^3", linewidth = 3)
+plt.title("Función en python con numpy y matplotlib")
+plt.legend()
 plt.grid()
 plt.show()
 
